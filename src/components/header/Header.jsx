8e635f4317ce5abeb0446442f87ea4d851930e2e
Klohne <enzo.vincent.basic@gmail.com>
2024-02-18 10:18:16 +0100

    Ajustements

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,35 @@
-import { NavLink } from 'react-router-dom'
-import React, { useState } from 'react';
-import logo from '../../assets/logo.png'
-import './Header.css'
+import { NavLink, useLocation } from 'react-router-dom';
+import React from 'react';
+import logo from '../../assets/logo.png';
+import './Header.css';
 
 function Header(){
-    const [activePage, setActivePage] = useState('Home'); // État pour stocker la page active
-        return (
+    const location = useLocation(); // Obtenir l'URL actuelle. ( Renvoie un objet 'location', ex : pathname)
+    const [activePage, setActivePage] = React.useState(location.pathname); // On gère ici l'état local 'activePage' ( URL active ). Sa valeur est 'location.pathname' ( l'URL de notre page )
+
+    React.useEffect(() => {
+        setActivePage(location.pathname); // On utilise useEffect pour effectuer une action dès que l'URL ([location]) change. Le state est donc mis à jour (setActivePage) avec le nouveau chemin (location.pathname)
+    }, [location]);
+
+    return (
         <header>
             <img src={logo} alt="Logo de Kasa" />
             <div className='nav-links'>
-            <NavLink
-                to="/"
-                className={activePage === 'Home' ? 'active' : ''}
-                onClick={() => setActivePage('Home')}>
-                <li>Accueil</li>
-            </NavLink>
-            <NavLink
-                to="/About"
-                className={activePage === 'About' ? 'active' : ''}
-                onClick={() => setActivePage('About')}>
-                <li>A-propos</li>
-            </NavLink>
+                <NavLink
+                    to="/"
+                    onClick={() => setActivePage('/')}
+                    className={activePage === '/' ? 'active' : ''}>
+                    <li>Accueil</li>
+                </NavLink>
+                <NavLink
+                    to="/About"
+                    onClick={() => setActivePage('/About')}
+                    className={activePage === '/About' ? 'active' : ''}>
+                    <li>A-propos</li>
+                </NavLink>
             </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
