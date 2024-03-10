1766c619f93052196a7e1c0b8925737cca12b08b
Klohne <enzo.vincent.basic@gmail.com>
2024-03-10 09:33:18 +0100

    build

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Logement from './pages/Logement'
 
 function App() {
   return (
-      <BrowserRouter basename='/Kasa'>
+      <BrowserRouter>
           <Header />
           <Routes>
               <Route path="/" element={<Home />} />
